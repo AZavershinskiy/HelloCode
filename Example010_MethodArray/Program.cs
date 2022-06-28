90e2b42ad12b8cfa154b31c820e3ac505c4eefa3
A_Zavershinskiy_Win7 <yeah-men@mail.ru>
2022-06-28 12:58:17 +0200

    add Example 010 Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿//              0   1   2  3   4   5   6   7   8
+int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // останавливает алгоритм поиска индекса нужного элемента после первого совпадения
+    }
+    /* index = index + 1;  */// или так
+    index++;
+}
