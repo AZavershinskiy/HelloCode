275067cbb836423e30da2356be7f09523d935a18
A_Zavershinskiy_Win7 <yeah-men@mail.ru>
2022-07-13 13:28:10 +0200

    update Example 012

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -44,7 +44,7 @@ int year = Method3();
 
 //Вид4
 
-string Method4(int count, string text)
+/* string Method4(int count, string text)
 {
     int i = 0;
     string result = String.Empty;
@@ -55,7 +55,67 @@ string Method4(int count, string text)
         i++;
     }
     return result;
+} */
+
+//или
+
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(3, "Z ");
+//Console.WriteLine(res);
+
+
+
+/* for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+} */
+
+
+
+//===== Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить  большими "К",
+// а большие "С" заменить маленькими "с".
+
+// Ясна ли задача?
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винуенгероде, "
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+//string s = "qwerty"
+//            012345
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result += $"{newValue}";
+        else result += $"{text[i]}";
+    }
+    return result;
 }
 
-string res = Method4(10, "Z ");
-Console.WriteLine(res);
+string newText = Replace(text, ' ', '|');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
